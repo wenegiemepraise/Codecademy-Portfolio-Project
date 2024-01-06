4811efa27bce6f8dd1879732fa60a9996357ae3c
Praise Wenegieme <praisewenegieme29@gmail.com>
2024-01-06 20:35:39 +0000

    Create main.py
    Outlining what I need in the project.

---
+++ main.py
@@ -0,0 +1,12 @@
+#Who wants to be a millionaire
+print("Welcome to who wants to become a millionaire featuring your host 'Praise Wenegieme' ")
+print("In todays episode we will take on three challangers and let them compete against eachother for the grand prize of 1 million euro!!")
+#First get challanger information
+#Based on age set a specific questions
+#Asked them if they want a certain category but if they get to pick their category they lose 50% of the prize
+#Use time input import to track how long it takes to answer each question.
+#Pick challangers randomly in order for the questions
+#Allow each challanger 3 hints
+#Challanger coule be a potential object
+#They have to answer 10 or 15 questions and they can only get 2 or 3 wrong before they are disqualified
+#Give out extra additional prizes for speed completion
